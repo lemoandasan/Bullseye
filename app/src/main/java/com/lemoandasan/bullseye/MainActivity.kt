42972734ed5308daba260d2cf2fe104564f40bb3
Lem Oandasan <lem_oandasan@me.com>
2022-08-21 09:13:33 -0500

    show alert when hit me button is tapped
    add strings for dialog title and message

--- app/src/main/java/com/lemoandasan/bullseye/MainActivity.kt
+++ app/src/main/java/com/lemoandasan/bullseye/MainActivity.kt
@@ -2,10 +2,35 @@ package com.lemoandasan.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.lemoandasan.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        binding.hitMeButton.setOnClickListener {
+            Log.i("**** button click", "the hit me button was tapped")
+            showResult()
+        }
+    }
+
+    private fun showResult() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = getString(R.string.result_dialog_message)
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 }
