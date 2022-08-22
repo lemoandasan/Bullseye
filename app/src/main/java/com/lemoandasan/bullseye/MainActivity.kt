5e3883b68e3dc92ec0b9880c8e78698f46bbfec6
Lem Oandasan <lem_oandasan@me.com>
2022-08-22 09:49:58 -0500

    create app theme
    apply new theme and colors to the app

--- app/src/main/java/com/lemoandasan/bullseye/MainActivity.kt
+++ app/src/main/java/com/lemoandasan/bullseye/MainActivity.kt
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
     private var currentRound = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportActionBar?.hide()
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
